c50c7bf0cd34b9b67aa051fa7d03d5d79d1b1969
Elliot Berentsen <elliot@overlandexperten.com>
2026-01-18 16:50:54 +0100

    Initial Commit

---
+++ Toxic Tom/Toxic_TomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toxic_TomApp.swift
+//  Toxic Tom
+//
+//  Created by Elliot Berentsen on 2026-01-18.
+//
+
+import SwiftUI
+
+@main
+struct Toxic_TomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Toxic TomTests/Toxic_TomTests.swift
@@ -0,0 +1,16 @@
+//
+//  Toxic_TomTests.swift
+//  Toxic TomTests
+//
+//  Created by Elliot Berentsen on 2026-01-18.
+//
+
+import Testing
+
+struct Toxic_TomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
